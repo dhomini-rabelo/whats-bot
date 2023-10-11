6178821460c0c35548f2e53343a207d4a7d80971
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-10 22:56:53 -0300

    FEAT: start project with express

---
+++ src/core/server.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+
+
+const app = express()
+
+
+app.use(express.json());
+
+app.get('/', function (req, res) {
+  return res.status(200).json({
+    Hello: 'World'
+  })
+})
+
+app.listen(5001, () => {
+  console.log('Server running')
+})
