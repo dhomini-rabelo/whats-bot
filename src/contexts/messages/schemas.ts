628e20141d161e36c24a4f79c4f5a8c060ab45df
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-12 18:36:08 -0300

    CHORE: add flow

--- src/contexts/messages/schemas.ts
+++ src/contexts/messages/schemas.ts
@@ -6,8 +6,9 @@ export interface Contact {
 }
 
 export interface Question {
-  text: string
   key: string
+  description: string
+  message: string
 }
 
 export interface ChatResponse {
@@ -22,7 +23,7 @@ export interface ChatData {
   responses: Array<ChatResponse>
   startedAt: string
   updatedAt: string
-  customStatus: string
+  customStatus: 'started' | 'in progress' | 'finished' | string
   status: 'started' | 'in progress' | 'finished'
 }
 
@@ -61,8 +62,9 @@ export interface DefaultStep extends Question {
 
 export interface StartStep extends Omit<DefaultStep, 'type' | 'message'> {
   type: 'start-step'
-  startMessage: string
+  startMessage: string | null
   restartMessage: string | null
+  withResponse: boolean
 }
 
 export interface FinishStep extends Omit<DefaultStep, 'type' | 'options'> {
@@ -74,5 +76,25 @@ export interface Bot {
   name: string
   description: string
   steps: Record<string, DefaultStep | StartStep | FinishStep>
-  // expirationTime: number = 60
+  responseForError: string
+  // expirationTime: number = 60 * 60 - 1 hour
 }
+
+/*
+
+* FLUXO
+
+[client] Cliente envia primeira mensagem
+[backend] Recebe a mensagem, cria um Chat lincado com um bot
+[backend] Baseado na configuração do bot faz uma pergunta (StartStep | DefaultStep), que pode ser de resposta livre ou baseada em opções
+[client] Responde a pergunta, corretamente ou não
+[backend] Busca um Chat a partir do contato e do bot
+[backend] Processa a resposta, se necessário, irá reenviar
+[chat] Conversa continua, expira, finaliza ou reinicia
+
+* RESPOSTA GLOBAIS
+sair -> encerra a conversa
+reiniciar -> reinicia a conversa
+
+
+*/
