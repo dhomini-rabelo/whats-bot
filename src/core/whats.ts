fe33d613699f833e2d16f379d54d2f189ac18643
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-11 22:13:19 -0300

    FEAT: setup for @whiskeysockets/baileys

--- src/core/whats.ts
+++ src/core/whats.ts
@@ -1,19 +1,49 @@
-import { Message, Whatsapp, create } from 'venom-bot'
+import makeWASocket, {
+  DisconnectReason,
+  useMultiFileAuthState,
+} from '@whiskeysockets/baileys'
+import { Boom } from '@hapi/boom'
 
-create({
-  session: 'bot',
-  disableWelcome: true,
-})
-  .then(async (client: Whatsapp) => await start(client))
-  .catch((err) => {
-    console.log(err)
+async function connectToWhatsApp() {
+  const { state, saveCreds } = await useMultiFileAuthState('tokens')
+
+  const sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: true,
   })
 
-async function start(client: Whatsapp) {
-  client.onMessage(async (message: Message) => {
-    if (message.body && !message.isGroupMsg) {
-      console.log(`message: ${message.body}`)
-      await client.sendText(message.from, 'Olá')
+  sock.ev.on('creds.update', saveCreds)
+
+  sock.ev.on('connection.update', (update) => {
+    const { connection, lastDisconnect } = update
+    if (connection === 'close' && lastDisconnect) {
+      const shouldReconnect =
+        (lastDisconnect.error as Boom)?.output?.statusCode !==
+        DisconnectReason.loggedOut
+      console.log(
+        'connection closed due to ',
+        lastDisconnect.error,
+        ', reconnecting ',
+        shouldReconnect,
+      )
+      // reconnect if not logged out
+      if (shouldReconnect) {
+        connectToWhatsApp()
+      }
+    } else if (connection === 'open') {
+      console.log('opened connection')
     }
   })
+  sock.ev.on('messages.upsert', async (m) => {
+    console.log(JSON.stringify(m, undefined, 2))
+
+    console.log('replying to', m.messages[0].key.remoteJid)
+    if (m.messages[0].key.fromMe === true) return
+
+    await sock.sendMessage(m.messages[0].key.remoteJid!, {
+      text: 'Hello ',
+    })
+  })
 }
+
+connectToWhatsApp()
