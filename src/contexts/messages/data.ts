f39b06eb45abdfd3b47a5686535e312ebfaf59f2
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-14 21:07:17 -0300

    FEAT: implements concept tests

---
+++ src/contexts/messages/data.ts
@@ -0,0 +1,101 @@
+import { Bot, Chat } from './schemas'
+
+export const bots: Bot[] = [
+  {
+    name: 'John Doe',
+    description: 'Register',
+    responseForError: 'Vamos reiniciar o processo de cadastro',
+    steps: {
+      '1': {
+        type: 'start-step',
+        startMessage: 'Olá, boa noite!',
+        nextStepId: '2',
+      },
+      '2': {
+        type: 'text-step',
+        key: 'name',
+        description: 'Nome',
+        message: 'Qual seu nome?',
+        nextStepId: '3',
+      },
+      '3': {
+        type: 'options-step',
+        key: 'isAdult',
+        description: 'É adulto?',
+        message: 'Você tem mais de 18 anos?',
+        nextStepId: '4',
+        optionType: 'text',
+        options: [
+          {
+            response: 'SIM',
+          },
+          {
+            response: 'NÃO',
+          },
+        ],
+      },
+      '4': {
+        type: 'options-step',
+        key: 'cellPhoneType',
+        description: 'Marca do Celular',
+        message: 'Qual a marca do seu celular?',
+        optionType: 'numeric',
+        options: [
+          {
+            response: 'Android',
+            nextStepId: '5',
+          },
+          {
+            response: 'Iphone',
+            nextStepId: '6',
+          },
+          {
+            response: 'Outro',
+            nextStepId: '7',
+          },
+        ],
+      },
+      '5': {
+        type: 'options-step',
+        key: 'androidType',
+        description: 'Tipo android',
+        message: 'Qual o tipo de android do seu celular?',
+        optionType: 'numeric',
+        nextStepId: '7',
+        acceptText: true,
+        options: [
+          {
+            response: 'Samsung',
+          },
+          {
+            response: 'Motorola',
+          },
+          {
+            response: 'Outro',
+          },
+        ],
+      },
+      '6': {
+        type: 'options-step',
+        key: 'iphoneType',
+        description: 'Tipo Iphone',
+        message: 'Qual o número do seu iphone?',
+        optionType: 'numeric',
+        nextStepId: '7',
+        acceptText: true,
+        options: [
+          {
+            response: 'Motorola',
+          },
+          {
+            response: 'Outro',
+          },
+        ],
+      },
+      '7': {
+        type: 'finish-step',
+        message: 'Obrigado!',
+      },
+    },
+  },
+]
--- src/contexts/messages/listener.ts
+++ src/contexts/messages/listener.ts
@@ -3,10 +3,119 @@ import {
   IReplier,
   IWhatsAppListener,
 } from '../../application/dependencies/whatsapp-provider/interfaces/listener'
+import { bots } from './data'
+import {
+  Bot,
+  Chat,
+  FinishStep,
+  OptionsStep,
+  StartStep,
+  ProcessSteps,
+  TextStep,
+  StepTypes,
+  Steps,
+} from './schemas'
+
+const chats: Chat[] = []
 
 export class WhatsAppListener implements IWhatsAppListener {
   async onMessage(data: IOnMessageData, reply: IReplier) {
-    console.log(`received message: ${data.text}`)
-    await reply.withText('Hello')
+    const bot = bots[0]
+    let chat = this.findChat(data)
+    if (!chat) {
+      const firstStep = bot.steps['1'] as StartStep
+      chat = this.createChat(data, firstStep.nextStepId)
+      if (firstStep.startMessage) {
+        await reply.withText(firstStep.startMessage)
+      }
+      const nextStep = bot.steps[firstStep.nextStepId] as ProcessSteps
+      await this.handleNextStep(nextStep, reply)
+    } else {
+      const currentStep = bot.steps[chat.stepIndex] as ProcessSteps
+      const nextStepId = this.getNextStepId(currentStep, data)
+      if (nextStepId) {
+        const nextStep = bot.steps[nextStepId] as ProcessSteps | undefined
+        if (nextStep) {
+          chats[0].stepIndex = nextStepId
+          await this.handleNextStep(nextStep, reply)
+        }
+      }
+    }
+    console.log({ chats, data })
+  }
+
+  private getNextStepId(
+    currentStep: ProcessSteps,
+    data: IOnMessageData,
+  ): string | undefined {
+    if (currentStep.type === 'text-step') {
+      return currentStep.nextStepId
+    } else if (currentStep.type === 'options-step') {
+      if (currentStep.nextStepId) {
+        return currentStep.nextStepId
+      } else if (currentStep.optionType === 'numeric') {
+        const option = currentStep.options[data.text]
+        if (option && option.nextStepId) {
+          return option.nextStepId
+        }
+      } else if (currentStep.optionType === 'text') {
+        const option = currentStep.options.find(
+          (option) => option.response === data.text,
+        )
+        if (option && option.nextStepId) {
+          return option.nextStepId
+        }
+      }
+    }
+  }
+
+  private async handleNextStep(nextStep: ProcessSteps, reply: IReplier) {
+    if (nextStep.type === 'text-step') {
+      await reply.withText(nextStep.message)
+    } else if (nextStep.type === 'options-step') {
+      if (nextStep.optionType === 'numeric') {
+        await reply.withText(
+          `
+            ${nextStep.message}
+            ${nextStep.options
+              .map((option, index) => `${index} - ${option.response}`)
+              .join('\n')}
+          `.trim(),
+        )
+      } else if (nextStep.optionType === 'text') {
+        await reply.withText(
+          `
+            ${nextStep.message}
+            ${nextStep.options.map((option) => `${option.response}`).join('\n')}
+          `.trim(),
+        )
+      }
+    } else if (nextStep.type === 'finish-step') {
+      await reply.withText(nextStep.message)
+    }
+  }
+
+  private findChat(data: IOnMessageData): Chat | undefined {
+    return chats.find((chat) => chat.contact.phone === data.contact.phone)
+  }
+
+  private createChat(data: IOnMessageData, nextStepId: string): Chat {
+    const chat: Chat = {
+      botId: '1',
+      contact: {
+        name: data.contact.name,
+        phone: data.contact.phone,
+      },
+      stepIndex: nextStepId,
+      currentChat: {
+        responses: [],
+        customStatus: 'stated',
+        status: 'started',
+        startedAt: '1',
+        updatedAt: '2',
+      },
+    }
+    chats.push(chat)
+    return chat
   }
 }
